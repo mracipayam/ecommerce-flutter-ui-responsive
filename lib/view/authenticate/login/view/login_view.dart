c6ef2be84f4c3a4d07dd8c2f008c9778f7c2b790
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-17 19:16:17 +0300

    Logo text and slogan added.

--- lib/view/authenticate/login/view/login_view.dart
+++ lib/view/authenticate/login/view/login_view.dart
@@ -1,7 +1,12 @@
 import 'package:ecommerceflutterapp/core/base/state/base_state.dart';
 import 'package:ecommerceflutterapp/core/base/widgets/base_widget.dart';
+import 'package:ecommerceflutterapp/core/components/appbars/appbar_transparent.dart';
 import 'package:ecommerceflutterapp/core/components/textfields/base_input.dart';
+import 'package:ecommerceflutterapp/core/components/textfields/search_input.dart';
+import 'package:ecommerceflutterapp/core/constants/app/app_constants.dart';
+import 'package:ecommerceflutterapp/core/init/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -9,14 +14,68 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends BaseState<LoginView> {
+  Widget loginViewContainer(Widget child) {
+    return Container(
+        height: dynamicHeight(1),
+        width: dynamicWidth(1),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(ApplicationConstants.IMAGES_PATH +
+                    "auth_background.webp"))),
+        child: child);
+  }
+
+  Widget textLogo() {
+    return Text(
+      "Audio",
+      style: TextStyle(
+          color: white,
+          fontWeight: FontWeight.bold,
+          fontSize: dynamicWidth(ApplicationConstants.TEXT_HEADER_L)),
+    );
+  }
+
+  Widget annotatedRegion(Widget child) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness: Brightness.light),
+      child: child,
+    );
+  }
+
+  Widget textSlogan() {
+    return Text(
+      "It's modular and designed to last",
+      style: TextStyle(
+          color: white,
+          fontSize: dynamicWidth(ApplicationConstants.TEXT_HEADER_S)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BaseView(
-      onModelReady: (value) {},
-      onPageBuilder: (context, value) {
-        return BaseInput();
-      },
-    ));
+      body: BaseView(
+        safeArea: false,
+        onPageBuilder: (context) {
+          return annotatedRegion(
+            loginViewContainer(
+              Column(
+                children: [
+                  textLogo(),
+                  textSlogan(),
+                  BaseInput(
+                    placeholder: "asdfsaf",
+                  ),
+                  SearchInput()
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }

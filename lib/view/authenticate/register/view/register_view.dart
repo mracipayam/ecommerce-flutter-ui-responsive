65a29cb91a24851be1796c9edc4de24bdb277262
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-18 00:58:17 +0300

    Register page added.

---
+++ lib/view/authenticate/register/view/register_view.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerceflutterapp/core/base/state/base_state.dart';
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  @override
+  _RegisterViewState createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends BaseState<RegisterView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}

a1832aed7a6c8e2d0e7e5f003121981314b8e27f
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-17 19:14:39 +0300

    Safe area bool added.

--- lib/core/base/widgets/base_widget.dart
+++ lib/core/base/widgets/base_widget.dart
@@ -2,54 +2,38 @@ import 'package:ecommerceflutterapp/core/base/state/base_state.dart';
 import 'package:flutter/material.dart';
 
 class BaseView<T> extends StatefulWidget {
-  final Widget Function(BuildContext context, T value) onPageBuilder;
-  final T viewModel;
-  final Function(T model) onModelReady;
-  final VoidCallback onDispose;
+  final Widget Function(BuildContext context) onPageBuilder;
+  final bool safeArea;
 
-  const BaseView(
-      {Key key,
-      this.viewModel,
-      @required this.onPageBuilder,
-      @required this.onModelReady,
-      this.onDispose})
-      : super(key: key);
+  const BaseView({
+    Key? key,
+    this.safeArea = true,
+    required this.onPageBuilder,
+  }) : super(key: key);
 
   @override
   _BaseViewState<T> createState() => _BaseViewState<T>();
 }
 
 class _BaseViewState<T> extends BaseState<BaseView<T>> {
-  T model;
-
   Widget scrollWidget(Widget child) {
     return SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(minHeight: dynamicHeight(1)),
-        child: child,
-      ),
+      child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: child),
     );
   }
 
-  Widget safeArea(Widget child) {
+  Widget safeAreaWidget(Widget child) {
     return SafeArea(child: child);
   }
 
   @override
-  void initState() {
-    model = widget.viewModel;
-    widget.onModelReady(model);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (widget.onDispose != null) widget.onDispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return safeArea(scrollWidget(widget.onPageBuilder(context, model)));
+    return widget.safeArea
+        ? safeAreaWidget(scrollWidget(widget.onPageBuilder(context)))
+        : scrollWidget(widget.onPageBuilder(context));
   }
 }

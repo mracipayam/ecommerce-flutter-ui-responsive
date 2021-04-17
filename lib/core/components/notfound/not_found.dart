5c78ffd2aa71544b52120cb00d5ed6222651250a
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-18 00:55:12 +0300

    Not found component created.

---
+++ lib/core/components/notfound/not_found.dart
@@ -0,0 +1,11 @@
+import '../texts/text_medium.dart';
+import 'package:flutter/material.dart';
+
+class NotFound extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TextLarge(text: "Page not found!"),
+    );
+  }
+}

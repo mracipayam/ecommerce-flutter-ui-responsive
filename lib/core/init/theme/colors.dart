af0b6978a8ed6bc53af3cf646d92766ee14c4f48
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-17 19:15:37 +0300

    Colors added.

---
+++ lib/core/init/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+final black = Color(0xFF000000);
+final greyDark = Color(0xFF7F7F7F);
+final grey = Color(0xFFBABABA);
+final lightGrey = Color(0xFFF3F3F3);
+final lightGrey2 = Color(0xFFF6F6F6);
+final white = Color(0xFFFFFFFF);
+final primary = Color(0xFF0ACF83);
+final accent = Color(0xFFFFC120);

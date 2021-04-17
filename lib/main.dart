9822fe124419f3d350ac1451421947987e51263b
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-17 19:15:50 +0300

    ThemeData props changed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerceflutterapp/core/init/theme/colors.dart';
 import 'package:ecommerceflutterapp/view/authenticate/login/view/login_view.dart';
 import 'package:flutter/material.dart';
 
@@ -10,18 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(fontFamily: 'DMSans', accentColor: grey),
       home: LoginView(),
     );
   }

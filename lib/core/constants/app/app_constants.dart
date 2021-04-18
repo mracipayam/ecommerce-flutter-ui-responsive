481e017b9da1542ba7732f7208f2a0508ea5e6fb
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-18 20:58:08 +0300

    New constants added.

--- lib/core/constants/app/app_constants.dart
+++ lib/core/constants/app/app_constants.dart
@@ -2,6 +2,7 @@ class ApplicationConstants {
   static const FONT_FAMILY = 'DMSans';
   static const SVG_PATH = "assets/svg/icons/";
   static const IMAGES_PATH = "assets/images/";
+  static const LOGOS_PATH = "assets/logos/";
   static const TEXT_HEADER_L = 0.0615;
   static const TEXT_HEADER_M = 0.041;
   static const TEXT_HEADER_S = 0.0359;
--- lib/core/constants/component/button_constants.dart
+++ lib/core/constants/component/button_constants.dart
@@ -2,4 +2,6 @@ class ButtonConstants {
   static const LARGE_BUTTON_WIDTH = 0.836;
   static const LARGE_BUTTON_HEIGHT = 0.0657;
   static const BUTTON_BORDER_RADIUS = 0.0256;
+  static const LOGO_BUTTON_WIDTH = 0.133;
+  static const LOGO_BUTTON_HEIGHT = 0.133;
 }

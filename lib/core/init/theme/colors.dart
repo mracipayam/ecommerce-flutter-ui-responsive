0ba650809242daa676bab631cc844c1a98276432
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-19 05:33:04 +0300

    new color added.

--- lib/core/init/theme/colors.dart
+++ lib/core/init/theme/colors.dart
@@ -8,3 +8,4 @@ final lightGrey2 = Color(0xFFF6F6F6);
 final white = Color(0xFFFFFFFF);
 final primary = Color(0xFF0ACF83);
 final accent = Color(0xFFFFC120);
+final backgroundColor = Color(0xFFFFFFFF);

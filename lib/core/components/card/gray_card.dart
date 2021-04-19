7e58fe610396171b1e2b2a46b999af73483ec711
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-19 05:31:20 +0300

    Grey Card added.

---
+++ lib/core/components/card/gray_card.dart
@@ -0,0 +1,28 @@
+import 'package:ecommerceflutterapp/core/base/state/base_state.dart';
+import 'package:ecommerceflutterapp/core/init/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+class GrayCard extends StatefulWidget {
+  final Widget child;
+
+  const GrayCard({Key? key, required this.child}) : super(key: key);
+  @override
+  _GrayCardState createState() => _GrayCardState();
+}
+
+class _GrayCardState extends BaseState<GrayCard> {
+  double cardRadius = 0.077;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: dynamicWidth(1),
+      padding: EdgeInsets.symmetric(vertical: dynamicWidth(0.1)),
+      decoration: BoxDecoration(
+          color: lightGrey,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(dynamicWidth(cardRadius)),
+              topRight: Radius.circular(dynamicWidth(cardRadius)))),
+      child: widget.child,
+    );
+  }
+}

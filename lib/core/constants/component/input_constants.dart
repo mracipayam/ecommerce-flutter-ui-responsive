d5a243d26d902cdd7b1de4799dabebb593372eb6
Mustafa Emin Bosnalı <mustafaeminbosnali@hotmail.com>
2021-04-20 03:31:28 +0300

    New input constants added.

--- lib/core/constants/component/input_constants.dart
+++ lib/core/constants/component/input_constants.dart
@@ -1,5 +1,5 @@
 class InputConstants {
-  static const LARGE_INPUT_WIDTH = 0.836;
+  static const LARGE_INPUT_WIDTH = 0.87;
   static const LARGE_INPUT_HEIGHT = 0.0657;
   static const INPUT_BORDER_RADIUS = 0.0256;
 }
